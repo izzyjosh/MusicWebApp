b4d6d6742bb13c2ba0b83d253e78efb9add2ddbd
izzyjosh <calebayuba222@gmail.com>
2023-12-04 23:10:49 +0100

    added url path for the function created for nexting music

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ app_name = "main"
 urlpatterns = [
         path("",views.music,name="music"),
         path("listen/<int:music_id>/",views.listen,name="listen"),
+        path("musiclist/",views.music_url_list,name="url_list"),
         ]
