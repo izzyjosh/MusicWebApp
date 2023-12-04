00d1b3f3718c6085831d963e01981dd3c7cf85a7
izzyjosh <calebayuba222@gmail.com>
2023-12-04 23:10:04 +0100

    added function for playing the nex music

--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Music
-from django.http import HttpRequest
+from django.http import HttpRequest,JsonResponse
 from django.db.models import Q
+from django.views.decorators.csrf import csrf_exempt
+from django.core.serializers import serialize
 
 
 def music(request:HttpRequest):
@@ -27,3 +29,35 @@ def listen(request:HttpRequest,music_id:int):
             }
     return render(request,"listen.html",context)
 
+
+@csrf_exempt
+def music_url_list(request:HttpRequest):
+    if request.method == "POST":
+        music_url = request.POST.get("music_url")
+        status = request.POST.get("status")
+        print(music_url)
+        print(status)
+
+        current_music = Music.objects.get(music_file=music_url)
+        if status == "next":
+            print(True)
+            next_music_id = current_music.id + 1
+            music = Music.objects.get(id=next_music_id)
+            
+            music_data = {
+                    "id": music.id,
+                    "title": music.title,
+                    "musician":music.musician,
+                    "image": music.image.url,
+                    "music_url": music.music_file.url,
+                    }
+
+        elif status == "previous":
+            previous_music_id = current_music.id + 1
+            try:
+                music = Music.objects.get(id=previous_music_id)
+            except Music.DoesNotExist:
+                music = current_music
+
+
+        return JsonResponse(music_data)
