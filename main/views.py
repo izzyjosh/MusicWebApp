a6fdf981e4a65c79cc560607db3dda8e99c370fa
izzyjosh <calebayuba222@gmail.com>
2023-12-10 13:57:11 +0100

    added a page for user to upload music

--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Music
 from django.http import HttpRequest,JsonResponse
 from django.db.models import Q
 from django.views.decorators.csrf import csrf_exempt
 from django.core.serializers import serialize
+from .forms import AddMusicForm
 
 
 def music(request:HttpRequest):
@@ -45,6 +46,7 @@ def music_url_list(request:HttpRequest):
                 music = Music.objects.get(id=next_music_id)
             except Music.DoesNotExist:
                 music = current_music
+                end = True
 
         elif status == "previous":
             previous_music_id = current_music.id + int(count)
@@ -52,6 +54,7 @@ def music_url_list(request:HttpRequest):
                 music = Music.objects.get(id=previous_music_id)
             except Music.DoesNotExist:
                 music = current_music
+                end = True
 
         music_data = {
                 "id": music.id,
@@ -59,7 +62,23 @@ def music_url_list(request:HttpRequest):
                 "musician":music.musician,
                 "image": music.image.url,
                 "music_url": music.music_file.url,
+                "end": end,
                 }
 
 
         return JsonResponse(music_data)
+
+
+
+def addmusic(request:HttpRequest):
+    if request.method == "POST":
+        form = AddMusicForm(request.POST,request.FILES)
+
+        if form.is_valid():
+            form.save()
+            return redirect("main:music")
+    else:
+        form = AddMusicForm()
+
+
+    return render(request,"addmusic.html",{"form":form})
