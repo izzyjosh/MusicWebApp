6ed2707dcbdc36869ec874135aca7ff35f0930b0
izzyjosh <calebayuba222@gmail.com>
2023-12-17 23:45:18 +0100

    Corrected python version

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,5 @@
 # build_files.sh
 
 pip install -r requirements.txt
-PYTHON=python3.11.6
-python3.11.6 manage.py collectstatic --no-input --clear
+python3 manage.py collectstatic --no-input --clear
 
